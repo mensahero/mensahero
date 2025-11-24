a55a2a8d0e79934d0428328bd9b2fbf8fa001368
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-11-24 14:47:33 +0800

    style: Remove extra blank lines

--- app/Models/Concerns/HasTeams.php
+++ app/Models/Concerns/HasTeams.php
@@ -7,15 +7,11 @@ use Jurager\Teams\Traits\HasTeams as BaseHasTeams;
 
 trait HasTeams
 {
-
     use BaseHasTeams;
-
 
     public function markAsDefaultTeam(Team $team): void
     {
         $team->default = true;
         $team->save();
     }
-
-
 }
