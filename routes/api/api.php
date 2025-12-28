7ebd9c80ecddfe20536e318ffe3a87d1a00e078f
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-12-28 13:48:00 +0800

    feat: Add health check and fallback routes

--- routes/api/api.php
+++ routes/api/api.php
@@ -1,5 +1,18 @@
 <?php
 
+use App\Facades\Api;
+
 require __DIR__.'/auth/auth.php';
 require __DIR__.'/profile/user.php';
 require __DIR__.'/modules/devices.php';
+
+
+Route::get('/health', function () {
+    try {
+        return 'OK';
+    } catch (\Throwable $th) {
+        return $th;
+    }
+});
+
+Route::fallback(fn () => Api::error('Path not found', 404));
